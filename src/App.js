be98cd610fadd79b24affda3ed05ba74b11af71b
Raviteja Giduturi <rgiduturi@microsoft.com>
2021-10-26 17:31:48 +0530

    use effect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { UseEffect } from "./hooks/use-effect";
 import { UseReducer } from "./hooks/use-reducer";
 import { UseState } from "./hooks/use-state";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <UseState />
       <UseReducer />
+      <UseEffect />
     </div>
   );
 }
---
+++ src/hooks/use-effect.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const fetchData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  return await res.json();
+};
+
+export const UseEffect = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((response) => {
+      setData(response);
+    });
+  }, []);
+
+  return (
+    <div>
+      <fieldset>
+        <legend>Use Effect:</legend>
+        <ul>
+          {data.slice(0, 3).map((item) => (
+            <li> {item.title}</li>
+          ))}
+        </ul>
+      </fieldset>
+    </div>
+  );
+};
